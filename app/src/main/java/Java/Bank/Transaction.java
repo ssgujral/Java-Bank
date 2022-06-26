632baa4b657f51b0d670c8241f26715fa65d66b0
samgujral <71089547+samgujral@users.noreply.github.com>
2022-06-27 00:17:48 +0200

    Transaction class created

---
+++ app/src/main/java/Java/Bank/Transaction.java
@@ -0,0 +1,49 @@
+package Java.Bank;
+
+import java.time.LocalDateTime;
+
+import java.time.Clock;
+
+enum TransactionType{
+  DEPOSIT,
+  WITHDRAWAL
+}
+
+
+public class Transaction {
+
+  private float amount;
+  private LocalDateTime date;
+  private TransactionType type;
+
+  public Transaction(float amount, TransactionType type){
+    this(amount, type, Clock.systemDefaultZone());
+  }
+
+public Transaction(float amount, TransactionType type, Clock clock){
+
+  this.amount = amount;
+  this.type = type;
+  this.date = LocalDateTime.now(clock);
+
+}
+
+public float getAmout(){
+  return this.amount;
+
+}
+
+public LocalDateTime getDate(){
+
+  return this.date;
+
+}
+
+
+public TransactionType getType(){
+
+  return this.type;
+
+}
+  
+}
